eeefa396c4df29539a0d17f0bf66f9a6650940b7
isayaksh <isayaksh@gmail.com>
2023-09-24 15:39:33 +0900

    init <김상현> init project

---
+++ src/test/java/com/sc/suverymarket/SuveryMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.suverymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuveryMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
