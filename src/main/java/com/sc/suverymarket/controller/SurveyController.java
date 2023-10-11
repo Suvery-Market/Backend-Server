4686d12026eceb710a9c74b6b77d8390cce5c679
isayaksh <isayaksh@gmail.com>
2023-10-11 20:17:59 +0900

    feat <김상현 #7> Survey Controller 구현

--- src/main/java/com/sc/suverymarket/controller/SurveyController.java
+++ src/main/java/com/sc/suverymarket/controller/SurveyController.java
@@ -1,16 +1,9 @@
 package com.sc.suverymarket.controller;
 
-import com.sc.suverymarket.dto.survey.SurveyReqDto;
-import com.sc.suverymarket.dto.survey.SurveyReqDto.SurveyRegisterReqDto;
+import com.sc.suverymarket.dto.survey.SurveyRegisterReqDto;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.HttpStatus.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/surveys")
@@ -18,7 +11,7 @@ public class SurveyController {
 
     @PostMapping
     public ResponseEntity register(@RequestBody @Valid SurveyRegisterReqDto surveyRegisterReqDto) {
-        return new ResponseEntity<>(null, OK);
+        return ResponseEntity.ok(null);
     }
 
 }
