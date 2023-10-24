d26ffdca7d5affedbe00f4d6ae46fb0ae8862561
isayaksh <isayaksh@gmail.com>
2023-10-24 17:31:13 +0900

    feat <김상현 #9> add questionList to Survey entity

--- src/main/java/com/sc/suverymarket/entity/Survey.java
+++ src/main/java/com/sc/suverymarket/entity/Survey.java
@@ -9,7 +9,10 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -31,6 +34,9 @@ public class Survey {
     @Column(nullable = false, length = 100)
     private String title;
 
+    @OneToMany(mappedBy = "survey", cascade = ALL)
+    private List<Question> questionList = new ArrayList<>();
+
     @CreatedDate // Insert
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
@@ -40,10 +46,11 @@ public class Survey {
     private LocalDateTime modifiedAt;
 
     @Builder
-    public Survey(Long id, User user, String title, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public Survey(Long id, User user, String title, List<Question> questionList, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id = id;
         this.user = user;
         this.title = title;
+        this.questionList = questionList;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
     }
