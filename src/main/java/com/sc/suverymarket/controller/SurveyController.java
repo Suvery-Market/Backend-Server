27a1f0355e4601b612a3ad5b51bec9116b104a95
isayaksh <isayaksh@gmail.com>
2023-10-24 18:00:13 +0900

    feat <김상현 #9> modify survey controller

--- src/main/java/com/sc/suverymarket/controller/SurveyController.java
+++ src/main/java/com/sc/suverymarket/controller/SurveyController.java
@@ -1,17 +1,31 @@
 package com.sc.suverymarket.controller;
 
+import com.sc.suverymarket.dto.ResponseDto;
 import com.sc.suverymarket.dto.survey.SurveyRegisterReqDto;
+import com.sc.suverymarket.dto.survey.SurveyResDto;
+import com.sc.suverymarket.service.SurveyService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/surveys")
+@RequiredArgsConstructor
 public class SurveyController {
+
+    private final SurveyService surveyService;
 
     @PostMapping
     public ResponseEntity register(@RequestBody @Valid SurveyRegisterReqDto surveyRegisterReqDto) {
-        return ResponseEntity.ok(null);
+
+        SurveyResDto surveyResDto = surveyService.register(surveyRegisterReqDto);
+
+        return ResponseEntity
+                .ok(ResponseDto.builder()
+                        .code(1)
+                        .message("Success")
+                        .data(surveyResDto));
     }
 
 }
